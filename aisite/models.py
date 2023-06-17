ddfd1d6c2611e5647246520b1281bd9ae3f75aea
aryan <aryancomp0106@gmail.com>
2023-06-17 18:32:47 +0430

    completing about me

--- aisite/models.py
+++ aisite/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Site(models.Model):
     title = models.CharField(max_length=200, verbose_name="عنوان")
     description = models.TextField(verbose_name="توضیحات")
     image = models.ImageField(upload_to='sites', verbose_name="تصویر")
+
+    def get_absolute_url(self):
+        return reverse('site_detail', args=[str(self.id)])
 
     def __str__(self):
         return self.title
